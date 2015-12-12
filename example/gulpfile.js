5c6ea7a4daf7051104eaa57be1ec922c99e9375b
Camilo Tapia <camilo.tapia@24hr.se>
2015-12-12 16:52:36 +0100

    bump

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,22 +2,12 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const postcssSpacing = require('../');
 
-function handleError (err) {
-    console.log('ERROR');
-    console.log(err.fileName ? err.fileName : '');
-    console.log(err.stack);
-
-
-    // emit end to allow the stream to recover
-    this.emit('end');
-}
-
 gulp.task('css', () => {
     return gulp.src('./src/**/*.css')
         .pipe(postcss([
             postcssSpacing()
         ]))
-        .on('error', handleError)
+        .on('error', console.error)
         .pipe(gulp.dest('./dist'));
 });
 
